55dae2f3bfd5424ae25d25c0bfe2ce67a4902f22
Barr <barr@barrisrael.com>
2024-12-04 10:42:43 +0200

    day 4

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let s = read_to_string("./input/2024/day4.txt").unwrap();
     let input = s.as_bytes();
     for _ in 0..10000 {
-        sum += day4::part1(&input[..input.len() - 1]) as u64;
+        sum += day4::part1_naive(&input[..input.len() - 1]) as u64;
         //         sum += day4::part1(
         //             b"MMMSXXMASM
         // MSAMXMSMSA
