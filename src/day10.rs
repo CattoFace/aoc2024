eb2c4da19fa1768c428fefd53e81bb075eaad07e
Barr <barr@barrisrael.com>
2024-12-10 13:03:41 +0200

    day 10

--- src/day10.rs
+++ src/day10.rs
@@ -4,12 +4,43 @@ use aoc_runner_derive::aoc;
 
 pub fn part1(input: &str) -> u32 {
     let input = input.as_bytes();
-    part1_buffer(&input[..input.len() - 1])
+    part1_buffer_backwards(&input[..input.len() - 1])
 }
 
 pub fn part2(input: &str) -> u32 {
     let input = input.as_bytes();
-    part2_buffer(&input[..input.len() - 1])
+    part2_buffer_backwards(&input[..input.len() - 1])
+}
+
+fn find_trailhead_rating_buffer_backwards(
+    input: &[u8],
+    start: usize,
+    width: usize,
+    height: usize,
+    queue: &mut Vec<(usize, usize, usize, u8)>,
+) -> u32 {
+    let mut score = 0u32;
+    queue.push((start, start % width, start / width, b'9'));
+    while let Some((curr, curr_x, curr_y, depth)) = queue.pop() {
+        // trail end reached
+        if depth == b'0' {
+            score += 1;
+            continue;
+        }
+        if curr_x > 0 && input[curr - 1] == depth - 1 {
+            queue.push((curr - 1, curr_x - 1, curr_y, depth - 1));
+        }
+        if curr_x < width - 2 && input[curr + 1] == depth - 1 {
+            queue.push((curr + 1, curr_x + 1, curr_y, depth - 1));
+        }
+        if curr_y > 0 && input[curr - width] == depth - 1 {
+            queue.push((curr - width, curr_x, curr_y - 1, depth - 1));
+        }
+        if curr_y < height && input[curr + width] == depth - 1 {
+            queue.push((curr + width, curr_x, curr_y + 1, depth - 1));
+        }
+    }
+    score
 }
 
 fn find_trailhead_rating_buffer(
@@ -64,6 +95,43 @@ fn find_trailhead_rating(input: &[u8], start: usize, width: usize, height: usize
         }
         if curr_y < height && input[curr + width] == depth + 1 {
             queue.push((curr + width, curr_x, curr_y + 1, depth + 1));
+        }
+    }
+    score
+}
+
+fn find_trailhead_score_buffer_backwards(
+    input: &[u8],
+    start: usize,
+    width: usize,
+    height: usize,
+    queue: &mut Vec<(usize, usize, usize, u8)>,
+) -> u32 {
+    let mut visited = bitvec![0; input.len()];
+    let mut score = 0u32;
+    queue.push((start, start % width, start / width, b'9'));
+    visited.set(start, true);
+    while let Some((curr, curr_x, curr_y, depth)) = queue.pop() {
+        // trail end reached
+        if depth == b'0' {
+            score += 1;
+            continue;
+        }
+        if curr_x > 0 && input[curr - 1] == depth - 1 && !visited[curr - 1] {
+            visited.set(curr - 1, true);
+            queue.push((curr - 1, curr_x - 1, curr_y, depth - 1));
+        }
+        if curr_x < width - 2 && input[curr + 1] == depth - 1 && !visited[curr + 1] {
+            visited.set(curr + 1, true);
+            queue.push((curr + 1, curr_x + 1, curr_y, depth - 1));
+        }
+        if curr_y > 0 && input[curr - width] == depth - 1 && !visited[curr - width] {
+            visited.set(curr - width, true);
+            queue.push((curr - width, curr_x, curr_y - 1, depth - 1));
+        }
+        if curr_y < height && input[curr + width] == depth - 1 && !visited[curr + width] {
+            visited.set(curr + width, true);
+            queue.push((curr + width, curr_x, curr_y + 1, depth - 1));
         }
     }
     score
@@ -138,6 +206,18 @@ fn find_trailhead_score(input: &[u8], start: usize, width: usize, height: usize)
     score
 }
 
+#[aoc(day10, part1, buffer_backwards)]
+pub fn part1_buffer_backwards(input: &[u8]) -> u32 {
+    let width = input.iter().position(|&c| c == b'\n').unwrap() + 1;
+    let height = input.len() / width;
+    let mut buffer = Vec::new();
+    memchr::memrchr_iter(b'9', input)
+        .map(|start| {
+            find_trailhead_score_buffer_backwards(input, start, width, height, &mut buffer)
+        })
+        .sum()
+}
+
 #[aoc(day10, part1, buffer)]
 pub fn part1_buffer(input: &[u8]) -> u32 {
     let width = input.iter().position(|&c| c == b'\n').unwrap() + 1;
@@ -154,6 +234,18 @@ pub fn part1_first(input: &[u8]) -> u32 {
     let height = input.len() / width;
     memchr::memrchr_iter(b'0', input)
         .map(|start| find_trailhead_score(input, start, width, height))
+        .sum()
+}
+
+#[aoc(day10, part2, buffer_backwards)]
+pub fn part2_buffer_backwards(input: &[u8]) -> u32 {
+    let width = input.iter().position(|&c| c == b'\n').unwrap() + 1;
+    let height = input.len() / width;
+    let mut buffer = Vec::new();
+    memchr::memrchr_iter(b'9', input)
+        .map(|start| {
+            find_trailhead_rating_buffer_backwards(input, start, width, height, &mut buffer)
+        })
         .sum()
 }
 
