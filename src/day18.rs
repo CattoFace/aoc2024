1e75d5b7d790293c577ae6442f5a3b3fbb27ce96
Barr <barr@barrisrael.com>
2024-12-18 22:18:28 +0200

    day 18

--- src/day18.rs
+++ src/day18.rs
@@ -12,7 +12,42 @@ pub fn part1(input: &str) -> u32 {
 
 pub fn part2(input: &str) -> String {
     let input = input.as_bytes();
-    part2_better(&input[..input.len() - 1])
+    part2_binsearch(&input[..input.len() - 1])
+}
+
+fn can_reach_end2(open_until: [u32; SIZE * SIZE], max_threshold: u32) -> bool {
+    let mut queue = Vec::new();
+    let mut queue_next = Vec::new();
+    queue.push((0usize, 0usize, 0usize));
+    let mut visited = [false; SIZE * SIZE];
+    visited[0] = true;
+    loop {
+        while let Some((pos, pos_x, pos_y)) = queue.pop() {
+            if pos == SIZE * SIZE - 1 {
+                return true;
+            }
+            if pos_x > 0 && max_threshold < open_until[pos - 1] && !visited[pos - 1] {
+                visited[pos - 1] = true;
+                queue_next.push((pos - 1, pos_x - 1, pos_y));
+            }
+            if pos_x < SIZE - 1 && max_threshold < open_until[pos + 1] && !visited[pos + 1] {
+                visited[pos + 1] = true;
+                queue_next.push((pos + 1, pos_x + 1, pos_y));
+            }
+            if pos_y > 0 && max_threshold < open_until[pos - SIZE] && !visited[pos - SIZE] {
+                visited[pos - SIZE] = true;
+                queue_next.push((pos - SIZE, pos_x, pos_y - 1));
+            }
+            if pos_y < SIZE - 1 && max_threshold < open_until[pos + SIZE] && !visited[pos + SIZE] {
+                visited[pos + SIZE] = true;
+                queue_next.push((pos + SIZE, pos_x, pos_y + 1));
+            }
+        }
+        if queue_next.is_empty() {
+            return false;
+        }
+        std::mem::swap(&mut queue, &mut queue_next);
+    }
 }
 fn can_reach_end(map: [bool; SIZE * SIZE]) -> bool {
     let mut queue = Vec::new();
@@ -49,6 +84,11 @@ fn can_reach_end(map: [bool; SIZE * SIZE]) -> bool {
     }
 }
 
+#[aoc(day18, part2, binary_search)]
+pub fn part2_binsearch(input: &[u8]) -> String {
+    let (x, y) = part2_binsearch_inner(input);
+    format!("{x},{y}")
+}
 #[aoc(day18, part2, better)]
 pub fn part2_better(input: &[u8]) -> String {
     let (x, y) = part2_better_inner(input);
@@ -59,6 +99,36 @@ pub fn part2_better(input: &[u8]) -> String {
 pub fn part2_first(input: &[u8]) -> String {
     let (x, y) = part2_first_inner(input);
     format!("{x},{y}")
+}
+
+fn part2_binsearch_inner(mut input: &[u8]) -> (usize, usize) {
+    let mut free_until = [u32::MAX; SIZE * SIZE];
+    let mut order = Vec::new();
+    for i in 0u32.. {
+        let (x, rem) = fast_parse::<usize>(input);
+        let (y, rem) = fast_parse::<usize>(&rem[1..]);
+        free_until[y * SIZE + x] = i;
+        order.push((x, y));
+        if rem.is_empty() {
+            break;
+        }
+        input = &rem[1..];
+    }
+    let result = perform_binsearch(free_until, order.len() as u32);
+    order[result as usize]
+}
+
+fn perform_binsearch(free_until: [u32; SIZE * SIZE], mut end: u32) -> u32 {
+    let mut start = 1024u32;
+    while start != end {
+        let middle = (start + end) / 2;
+        if can_reach_end2(free_until, middle) {
+            start = middle + 1;
+        } else {
+            end = middle;
+        }
+    }
+    end
 }
 
 fn part2_better_inner(mut input: &[u8]) -> (usize, usize) {
