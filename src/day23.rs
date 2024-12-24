3c9e64563eed5128aa50688721ded38d762a9f60
Barr <barr@barrisrael.com>
2024-12-24 09:52:24 +0200

    day 23

--- src/day23.rs
+++ src/day23.rs
@@ -1,8 +1,10 @@
 use std::array::from_fn;
 
 use aoc_runner_derive::aoc;
+use bitvec::prelude::*;
 use fxhash::FxHashSet;
 use itertools::Itertools;
+use tinyvec::ArrayVec;
 
 pub fn part1(input: &str) -> u32 {
     let input = input.as_bytes();
@@ -12,6 +14,37 @@ pub fn part1(input: &str) -> u32 {
 pub fn part2(input: &str) -> String {
     let input = input.as_bytes();
     part2_brute_force(&input[..input.len() - 1])
+}
+
+fn brute_force_clique_adj(
+    neighbours: [ArrayVec<[u32; 15]>; 26 * 26],
+    neighbours_adj: BitArr!(for 26*26*26*26),
+) -> Vec<u32> {
+    for (c1, n1) in neighbours.iter().enumerate() {
+        // looking specifically for cliques of size 13, vertices with less than 12 neighbours are
+        // irrelevant
+        if n1.len() < 12 {
+            continue;
+        }
+        // iterate over all combinations of 12 neighbours
+        let potential_cliques = n1
+            .iter()
+            .filter(|&&n| neighbours[n as usize].len() >= 12)
+            .copied()
+            .combinations(12);
+        for mut pc in potential_cliques {
+            // check if the current combination forms a clique
+            if pc
+                .iter()
+                .tuple_combinations()
+                .all(|(&a, &b)| neighbours_adj[(a * 26 * 26 + b) as usize])
+            {
+                pc.push(c1 as u32);
+                return pc;
+            }
+        }
+    }
+    unreachable!()
 }
 
 fn brute_force_clique(neighbours: [FxHashSet<usize>; 26 * 26]) -> Vec<usize> {
@@ -71,6 +104,31 @@ fn bron_kerbosch(
     max_set
 }
 
+#[aoc(day23, part2, adj)]
+pub fn part2_brute_adj(input: &[u8]) -> String {
+    let mut neighbours: [ArrayVec<[u32; 15]>; 26 * 26] = from_fn(|_| Default::default());
+    let mut neighbours_adj = bitarr![0usize;26*26*26*26];
+    input.chunks(6).for_each(|line: &[u8]| {
+        let comp1 = (line[0] - b'a') as u32 * 26 + (line[1] - b'a') as u32;
+        let comp2 = (line[3] - b'a') as u32 * 26 + (line[4] - b'a') as u32;
+        let adj1 = comp1 * 26 * 26 + comp2;
+        let adj2 = comp2 * 26 * 26 + comp1;
+        neighbours[comp1 as usize].push(comp2);
+        neighbours[comp2 as usize].push(comp1);
+        neighbours_adj.set(adj1 as usize, true);
+        neighbours_adj.set(adj2 as usize, true);
+    });
+    let max_set = brute_force_clique_adj(neighbours, neighbours_adj);
+    max_set
+        .iter()
+        .sorted()
+        .map(|&comp| {
+            String::from_utf8([(comp / 26) as u8 + b'a', (comp % 26) as u8 + b'a'].to_vec())
+                .unwrap()
+        })
+        .join(",")
+}
+
 #[aoc(day23, part2, brute_force)]
 pub fn part2_brute_force(input: &[u8]) -> String {
     let mut neighbours: [FxHashSet<usize>; 26 * 26] = from_fn(|_| Default::default());
@@ -116,10 +174,54 @@ pub fn part2_first(input: &[u8]) -> String {
         .join(",")
 }
 
+#[aoc(day23, part1, hashset)]
+pub fn part1_hashset(input: &[u8]) -> usize {
+    let mut neighbours: [FxHashSet<usize>; 26 * 26] = from_fn(|_| Default::default());
+    input.chunks(6).for_each(|line: &[u8]| {
+        // rotate letters to make t the smallest, for de-duplication later
+        let c11 = if line[0] < b't' {
+            line[0] + 26 - b't'
+        } else {
+            line[0] - b't'
+        };
+        let c12 = if line[1] < b't' {
+            line[1] + 26 - b't'
+        } else {
+            line[1] - b't'
+        };
+        let c21 = if line[3] < b't' {
+            line[3] + 26 - b't'
+        } else {
+            line[3] - b't'
+        };
+        let c22 = if line[4] < b't' {
+            line[4] + 26 - b't'
+        } else {
+            line[4] - b't'
+        };
+        let comp1 = c11 as usize * 26 + c12 as usize;
+        let comp2 = c21 as usize * 26 + c22 as usize;
+        // as a de-duplication policy, connections will be increasing IDs
+        if comp1 <= comp2 {
+            neighbours[comp1].insert(comp2);
+        } else {
+            neighbours[comp2].insert(comp1);
+        }
+    });
+    // only check computers that start with t
+    let mut count = 0usize;
+    for neighs in &neighbours[..26] {
+        for &comp2 in neighs {
+            count += neighs.intersection(&neighbours[comp2]).count();
+        }
+    }
+    count
+}
+
 #[aoc(day23, part1)]
 pub fn part1_first(input: &[u8]) -> u32 {
     let mut count = 0u32;
-    let mut neighbours: [Vec<usize>; 26 * 26] = from_fn(|_| Default::default());
+    let mut neighbours: [ArrayVec<[usize; 15]>; 26 * 26] = from_fn(|_| Default::default());
     input.chunks(6).for_each(|line: &[u8]| {
         // rotate letters to make t the smallest, for de-duplication later
         let c11 = if line[0] < b't' {
--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,36 @@
 use std::fs::read_to_string;
 
-use aoc2024::day21_cursed::Numpad::*;
-use aoc2024::day21_cursed::{self, code_to_amount};
-use aoc2024::day21_extra_cursed_25;
+use aoc2024::{day22, day23};
+
 fn main() {
-    // let mut sum1 = 0u32;
-    // let _s = read_to_string("./input/2024/day20.txt").unwrap();
-    // let b = _s.as_bytes();
-
-    // println!("{}", day21_cursed::code_to_amount(Zero, Two, Nine));
-    // println!("{}", day21_cursed::code_to_amount(Nine, Eight, Zero));
-    // println!("{}", day21_cursed::code_to_amount(One, Seven, Nine));
-    // println!("{}", day21_cursed::code_to_amount(Four, Five, Six));
-    // println!("{}", day21_cursed::code_to_amount(Three, Seven, Nine));
-    let s = 29 * day21_cursed::code_to_amount(Zero, Two, Nine, 25)
-        + 980 * day21_cursed::code_to_amount(Nine, Eight, Zero, 25)
-        + 179 * day21_cursed::code_to_amount(One, Seven, Nine, 25)
-        + 456 * day21_cursed::code_to_amount(Four, Five, Six, 25)
-        + 379 * day21_cursed::code_to_amount(Three, Seven, Nine, 25);
-    println!("{s}");
-    // let s2 = day21_extra_cursed_25::part2_single_lookup(29)
-    //     + day21_extra_cursed_25::part2_single_lookup(980)
-    //     + day21_extra_cursed_25::part2_single_lookup(179)
-    //     + day21_extra_cursed_25::part2_single_lookup(456)
-    //     + day21_extra_cursed_25::part2_single_lookup(379);
-    // println!("{s2}");
-    for i1 in 0u64..10 {
-        for i2 in 0u64..10 {
-            for i3 in 0u64..10 {
-                let num = i1 * 100 + i2 * 10 + i3;
-                let n1 = day21_cursed::i2n(i1);
-                let n2 = day21_cursed::i2n(i2);
-                let n3 = day21_cursed::i2n(i3);
-                let ans = num * code_to_amount(n1, n2, n3, 2);
-                println!("c_table[{num}] = {ans};");
-            }
-        }
+    let mut sum1 = 0u32;
+    let _s = read_to_string("./input/2024/day23.txt").unwrap();
+    let b = _s.as_bytes();
+    for _ in 0..10000 {
+        println!("{}", day23::part2_brute_adj(&b[..b.len() - 1]));
+        // sum += day23::part2_brute_force(&b[..b.len() - 1]) as u64;
+        //         sum += day20::part2_first_inner(
+        //             b"###############
+        // #...#...#.....#
+        // #.#.#.#.#.###.#
+        // #S#...#.#.#...#
+        // #######.#.#.###
+        // #######.#.#...#
+        // #######.#.###.#
+        // ###..E#...#...#
+        // ###.#######.###
+        // #...###...#...#
+        // #.#####.#.###.#
+        // #.#...#.#.#...#
+        // #.#.#.#.#.#.###
+        // #...#...#...###
+        // ###############",
+        //             50,
+        //         ) as u64;
+        //         sum += day19::part2_recursive(
+        //             b"r, wr, b, g, bwu, rb, gb, br
+        //
+        // rrbgbr",
+        //         ) as u64;
     }
-    return;
-
-    // for _ in 0..10 {
-    //     // println!("{}", day18::part2(&_s));
-    //     //
-    //     sum += day20::part2_first(&b[..b.len() - 1]) as u64;
-    //     //         sum += day20::part2_first_inner(
-    //     //             b"###############
-    //     // #...#...#.....#
-    //     // #.#.#.#.#.###.#
-    //     // #S#...#.#.#...#
-    //     // #######.#.#.###
-    //     // #######.#.#...#
-    //     // #######.#.###.#
-    //     // ###..E#...#...#
-    //     // ###.#######.###
-    //     // #...###...#...#
-    //     // #.#####.#.###.#
-    //     // #.#...#.#.#...#
-    //     // #.#.#.#.#.#.###
-    //     // #...#...#...###
-    //     // ###############",
-    //     //             50,
-    //     //         ) as u64;
-    //     //         sum += day19::part2_recursive(
-    //     //             b"r, wr, b, g, bwu, rb, gb, br
-    //     //
-    //     // rrbgbr",
-    //     //         ) as u64;
-    // }
 }
